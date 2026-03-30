49bfb20333ee9c9923595f0f0e9e54a599aebc77
CoCoral73 <114139368+CoCoral73@users.noreply.github.com>
2026-03-30 14:55:19 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 436 ms, Memory: 47812 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
@@ -1,88 +1,68 @@
+//
+//  main.cpp
+//  CppPractice
+//
+//  Created by 김정원 on 3/13/26.
+//
+
 #include <iostream>
-#include <sstream>
-#include <string>
 #include <vector>
-#include <queue>
 #include <map>
 #include <set>
 #include <algorithm>
-#include <cmath>
-#include <limits.h>
-#define int long long
-#define fastIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#include <queue>
+#define pii pair<int, int>
+
 using namespace std;
 
-typedef struct node {
-    int r, c;
-    int wall;
-    node(int r, int c, int wall) {
-        this->r = r;
-        this->c = c;
-        this->wall = wall;
-    }
-}NODE;
+const int INF = 2e9;
+int dr[4] = {-1, 0, 1, 0};
+int dc[4] = {0, 1, 0, -1};
 
-class BJ {
-    int N, M, K;
-    char grid[1000][1000];
-    int ***visited;
-    int dr[4] = {-1, 0, 1, 0};
-    int dc[4] = {0, 1, 0, -1};
-public:
-    BJ();
-    void bfs();
-};
+int N, M, K;
+vector<string> matrix;
+int isVisited[11][1000][1000];
 
-BJ::BJ() {
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
     cin >> N >> M >> K;
-    visited = new int **[N];
     for (int i = 0; i < N; i++) {
-        visited[i] = new int *[M];
-        for (int j = 0; j < M; j++) {
-            cin >> grid[i][j];
-            visited[i][j] = new int[K+1];
-            for (int k = 0; k <= K; k++)
-                visited[i][j][k] = INT32_MAX;
-        }
+        string str;
+        cin >> str;
+        matrix.push_back(str);
     }
-
-    bfs();
-    int answer = INT32_MAX;
-    for (int i = 0; i <= K; i++)
-        answer = min(answer, visited[N-1][M-1][i]);
-    cout << (answer == INT32_MAX ? -1 : answer);
-}
-void BJ::bfs() {
-    queue<NODE> Q;
-    Q.emplace(0, 0, 0);
-    visited[0][0][0] = 1;
-
-    while (!Q.empty()) {
-        NODE front = Q.front();
-        Q.pop();
-
+    
+    queue<pair<int, pii>> q;
+    q.push({0, {0, 0}});
+    isVisited[0][0][0] = 1;
+    
+    while (!q.empty()) {
+        int isBroken = q.front().first;
+        int y = q.front().second.first;
+        int x = q.front().second.second;
+        q.pop();
+        
+        if (y == N-1 && x == M-1) {
+            cout << isVisited[isBroken][y][x];
+            return 0;
+        }
+        
         for (int i = 0; i < 4; i++) {
-            int r = front.r + dr[i];
-            int c = front.c + dc[i];
+            int r = y + dr[i];
+            int c = x + dc[i];
+            
             if (r < 0 || r >= N || c < 0 || c >= M) continue;
-            if (grid[r][c] == '1') {
-                if (front.wall + 1 <= K && visited[r][c][front.wall + 1] > visited[front.r][front.c][front.wall] + 1) {
-                    visited[r][c][front.wall + 1] = visited[front.r][front.c][front.wall] + 1;
-                    Q.emplace(r, c, front.wall + 1);
-                }
-                else
-                    continue;
-            }
-            else if (visited[r][c][front.wall] > visited[front.r][front.c][front.wall] + 1){
-                visited[r][c][front.wall] = visited[front.r][front.c][front.wall] + 1;
-                Q.emplace(r, c, front.wall);
+            if (matrix[r][c] == '0' && !isVisited[isBroken][r][c]) {
+                isVisited[isBroken][r][c] = isVisited[isBroken][y][x] + 1;
+                q.push({isBroken, {r, c}});
+            } else if (matrix[r][c] == '1' && isBroken + 1 <= K && !isVisited[isBroken + 1][r][c]) {
+                isVisited[isBroken + 1][r][c] = isVisited[isBroken][y][x] + 1;
+                q.push({isBroken + 1, {r, c}});
             }
         }
     }
-}
-
-signed main() {
-    fastIO;
-
-    BJ Q14442;
+    
+    cout << -1;
 }
